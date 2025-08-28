acab959f61f1766a712ce8eadec5eda596caa084
Kalyan Kumar <pkalyankumar1010@gmail.com>
2025-08-28 21:50:24 +0530

    Added a functionality filter based on cibil score and to give loans to repayers

--- auto_lender.py
+++ auto_lender.py
@@ -23,6 +23,26 @@ try:
 except (TypeError, ValueError):
     print("Warning: Invalid LENDER_INTEREST_RATE, defaulting to 48.")
     LENDER_INTEREST_RATE_INNER = 48
+try:
+    LENDER_CIBIL_RATE = float(os.getenv("LENDER_CIBIL_RATE", "700"))
+except (TypeError, ValueError):
+    print("Warning: Invalid LENDER_CIBIL_RATE, defaulting to 700.")
+    LENDER_CIBIL_RATE = 700
+try:
+    LENDER_CIBIL_REPEATED_RATE = float(os.getenv("LENDER_CIBIL_REPEATED_RATE", "770"))
+except (TypeError, ValueError):
+    print("Warning: Invalid LENDER_CIBIL_REPEATED_RATE, defaulting to 770.")
+    LENDER_CIBIL_REPEATED_RATE = 770
+try:
+    LENDING_LOAN_AMOUNT = float(os.getenv("LENDING_LOAN_AMOUNT", "250"))
+except (TypeError, ValueError):
+    print("Warning: Invalid LENDING_LOAN_AMOUNT, defaulting to 250.")
+    LENDING_LOAN_AMOUNT = 250
+try:
+    LENDING_LOAN_REPEATED_AMOUNT = float(os.getenv("LENDING_LOAN_REPEATED_AMOUNT", "1000"))
+except (TypeError, ValueError):
+    print("Warning: Invalid LENDING_LOAN_REPEATED_AMOUNT, defaulting to 1000.")
+    LENDING_LOAN_REPEATED_AMOUNT = 1000
 GITHUB_HEADERS = {
     "Authorization": f"token {GIT_KEY}",
     "Accept": "application/vnd.github.v3+json"
@@ -30,7 +50,6 @@ GITHUB_HEADERS = {
 
 BODY_FETCH = {
   "filters": [
-    "tenure_2M"
   ],
   "sort_by": [
     "roi_high_low",
@@ -39,16 +58,41 @@ BODY_FETCH = {
   "partner_code": "LDC",
   "investor_id": INVESTOR_ID,
   "partner_id": "",
-  "limit": 10,
+  "limit": 100,
   "offset": 0,
   "loan_ids": []
 }
 
+BODY_FETCH_REP_LOANS = {
+  "filters": [
+    "repeated_loans_yes"
+  ],
+  "sort_by": [
+    "roi_high_low",
+    "tenure_low_high"
+  ],
+  "partner_code": "LDC",
+  "investor_id": INVESTOR_ID,
+  "partner_id": "",
+  "limit": 100,
+  "offset": 0,
+  "loan_ids": []
+}
 def limit_array(arr, limit=10):
     return arr[:limit] if len(arr) > limit else arr
 
 def fetch_loans():
     response = requests.request("POST",URL_FETCH, headers=HEADERS, json=BODY_FETCH)
+      # Debugging line to see the response content
+    # print("response content:", response.text)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Failed to fetch loans:", response.status_code)
+        return None
+
+def fetch_loans_rep():
+    response = requests.request("POST",URL_FETCH, headers=HEADERS, json=BODY_FETCH_REP_LOANS)
       # Debugging line to see the response content
     # print("response content:", response.text)
     if response.status_code == 200:
@@ -65,13 +109,25 @@ def fetch_balance():
     else:
         print("Failed to fetch balance:", response.status_code)
         return None
-def lend_to_loans(loan_roi_data,loans_lent,balance):
-    body = {
+def lend_to_loans(loan_roi_data,loans_lent,balance,repated_loans=False):
+    tmp_string =""
+    if repated_loans:
+        tmp_string = " (Repeated Loans)"
+        body = {
         "partner_code": "LDC",
         "investor_id": INVESTOR_ID,
         "loan_roi_data": loan_roi_data,
-        "lending_amount": 250
-    }
+        "lending_amount": LENDING_LOAN_REPEATED_AMOUNT
+        }
+    else:
+        body = {
+        "partner_code": "LDC",
+        "investor_id": INVESTOR_ID,
+        "loan_roi_data": loan_roi_data,
+        "lending_amount": LENDING_LOAN_AMOUNT
+        }
+
+
     print("Lending to loans:", loan_roi_data)
     response = requests.post(URL_LEND, headers=HEADERS, json=body)
     # print(response.text)
@@ -80,7 +136,7 @@ def lend_to_loans(loan_roi_data,loans_lent,balance):
         new_balance = new_bal_json["data"].get("account_balance", 0)
     if response.status_code == 200 and response.json().get("success")== 1:
         print("Successfully lent to loans:", loan_roi_data)
-        create_github_issue(f"✅ Lending Success : {loans_lent} loans", f"Balance before {balance} \nBalance after {new_balance} \nLent to loans: {loan_roi_data}")
+        create_github_issue(f"✅ Lending Success : {loans_lent} loans{tmp_string}", f"Balance before {balance} \nBalance after {new_balance} \nLent to loans{tmp_string}: {loan_roi_data}")
     else:
         tmp_response = response.json().get("message", "No message in response")
         print("Lending failed:", response.status_code, tmp_response)
@@ -119,7 +175,7 @@ def run():
         balance = bal_json["data"].get("account_balance", 0)
         # print(bal_json)
         print(f"Current balance: {balance}")
-        if balance < 250:
+        if balance < LENDING_LOAN_AMOUNT:
             print("Insufficient balance to lend. Waiting for next cycle.")
     # while True:
     print("Checking for eligible loans...")
@@ -141,17 +197,104 @@ def run():
 
         # if to_lend and balance >= 250:
         print(len(to_lend), f"loans found with ROI > {LENDER_INTEREST_RATE_INNER}.")
-        max_nunber_of_loans_to_lend = int(balance // 250)
+        max_nunber_of_loans_to_lend = int(balance // LENDING_LOAN_AMOUNT)
         print(f"Max number of loans to lend based on balance: {max_nunber_of_loans_to_lend}")
-        if balance >= 250 and len(to_lend) > 0:
+        if balance >= LENDING_LOAN_AMOUNT and len(to_lend) > 0:
             lend_to_loans(limit_array(to_lend, max_nunber_of_loans_to_lend),len(limit_array(to_lend, max_nunber_of_loans_to_lend)),balance)
         else:
-            print(f"No loans found with ROI > {LENDER_INTEREST_RATE_INNER} or Balance is < 250.")
+            print(f"No loans found with ROI > {LENDER_INTEREST_RATE_INNER} or Balance is < {LENDING_LOAN_AMOUNT}.")
     else:
         print("No data returned or error from API.")
 
     # print("Sleeping for 15 minutes...\n")
     # time.sleep(900)  # Sleep for 900 seconds (15 minutes)
+def fetch_score(loan_id):
+    url = f"https://investor-api.lendenclub.com/api/ims/retail-investor/v5/web/available-loan-details?investor_id={INVESTOR_ID}&partner_code=LDC&loan_id={loan_id}&default_amount=250"
+    response = requests.get(url, headers=HEADERS)
+    if response.status_code == 200:
+        data = response.json()
+        if data and data.get("success") == 1:
+            credit_score = data["data"].get("bureau", {}).get("score_range",0)
+            loan_details = data["data"].get("loan", {})
+            return credit_score
+        else:
+            return f"Failed to fetch loan details for Loan ID {loan_id}: {data.get('message', 'No message in response')}"
+    else:
+        return f"Failed to fetch loan details for Loan ID {loan_id}: HTTP {response.status_code}"
+def run2():
+    bal_json = fetch_balance()
+    if bal_json and bal_json.get("success") == 1:
+        balance = bal_json["data"].get("account_balance", 0)
+        # print(bal_json)
+        print(f"Current balance: {balance}")
+        if balance < LENDING_LOAN_AMOUNT:
+            print("Insufficient balance to lend. Waiting for next cycle.")
+    # while True:
+    print("Checking for eligible loans...")
+    data = fetch_loans()
+    if data and data.get("success") == 1:
+        loans = data["data"]["available_loans_list"]
+        to_lend = []
+        for loan in loans:
+            loan_id = loan["loan_id"]
+            loan_score = fetch_score(loan_id)
+            if float(loan_score) > LENDER_CIBIL_RATE:
+                try:
+                    roi = float(loan["loan_roi"])
+                    if roi >= LENDER_INTEREST_RATE_INNER:
+                        to_lend.append({
+                            "loan_id": loan["loan_id"],
+                            "loan_roi": "{:.2f}".format(roi)
+                        })
+                except Exception as e:
+                    print("Error processing loan:", e)
+        print(len(to_lend), f"loans found with ROI > {LENDER_INTEREST_RATE_INNER} and CIBIL > {LENDER_CIBIL_RATE}.")
+        max_nunber_of_loans_to_lend = int(balance // LENDING_LOAN_AMOUNT)
+        print(f"Max number of loans to lend based on balance: {max_nunber_of_loans_to_lend}")
+        if balance >= LENDING_LOAN_AMOUNT and len(to_lend) > 0:
+            lend_to_loans(limit_array(to_lend, max_nunber_of_loans_to_lend),len(limit_array(to_lend, max_nunber_of_loans_to_lend)),balance)
+        else:
+            print(f"No loans found with ROI > {LENDER_INTEREST_RATE_INNER} or Balance is < {LENDING_LOAN_AMOUNT}.")
+    else:
+        print("No data returned or error from API.")
 
+def run3():
+    bal_json = fetch_balance()
+    if bal_json and bal_json.get("success") == 1:
+        balance = bal_json["data"].get("account_balance", 0)
+        # print(bal_json)
+        print(f"Current balance: {balance}")
+        if balance < LENDING_LOAN_REPEATED_AMOUNT:
+            print("Insufficient balance to lend. Waiting for next cycle.")
+    # while True:
+    print("Checking for eligible loans...")
+    data = fetch_loans_rep()
+    if data and data.get("success") == 1:
+        loans = data["data"]["available_loans_list"]
+        to_lend = []
+        for loan in loans:
+            loan_id = loan["loan_id"]
+            loan_score = fetch_score(loan_id)
+            if float(loan_score) > LENDER_CIBIL_REPEATED_RATE:
+                try:
+                    roi = float(loan["loan_roi"])
+                    if roi >= LENDER_INTEREST_RATE_INNER:
+                        to_lend.append({
+                            "loan_id": loan["loan_id"],
+                            "loan_roi": "{:.2f}".format(roi)
+                        })
+                except Exception as e:
+                    print("Error processing loan:", e)
+        print(len(to_lend), f"repeated loans found with ROI > {LENDER_INTEREST_RATE_INNER} and CIBIL > {LENDER_CIBIL_REPEATED_RATE}.")
+        max_nunber_of_loans_to_lend = int(balance // LENDING_LOAN_REPEATED_AMOUNT)
+        print(f"Max number of loans to lend based on balance: {max_nunber_of_loans_to_lend}")
+        if balance >= LENDING_LOAN_REPEATED_AMOUNT and len(to_lend) > 0:
+            lend_to_loans(limit_array(to_lend, max_nunber_of_loans_to_lend),len(limit_array(to_lend, max_nunber_of_loans_to_lend)),balance,repated_loans=True)
+        else:
+            print(f"No loans found with ROI > {LENDER_INTEREST_RATE_INNER} or Balance is < {LENDING_LOAN_REPEATED_AMOUNT}.")
+    else:
+        print("No data returned or error from API.")
 if __name__ == "__main__":
-    run()
+    # run()
+    run2()
+    run3()
